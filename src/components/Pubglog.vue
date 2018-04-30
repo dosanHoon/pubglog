9a1483c72431c153513179391755e394e75b856b
danedehaan <danedehaan@qubitsec.com>
2018-04-30 12:46:42 +0900

    vue resource 추가 header 전역으로 설정 추후 변경 해야됨

--- src/components/Pubglog.vue
+++ src/components/Pubglog.vue
@@ -1,42 +1,63 @@
 <template>
 <div>
   <h1>{{ nickname }}</h1>
+  <h1>{{ country }}</h1>
+  <select v-model="country">
+    <option value="pc-krjp">krjp</option>
+    <option value="pc-as">as</option>
+  </select>
   <input v-model="nickname"/>
   <button v-on:click="searchLog">검색</button>
   </div>
 </template>
 
 <script>
-
-// var pubgApiUrl = "https://api.pubgtracker.com/v2/profile/pc/"
-var pubgApiUrl = "https://api.playbattlegrounds.com/shards/pc-krjp/players?filter[playerNames]="
-
 export default {
-  name: 'Login',
-  data () {
+  name: "Login",
+  data() {
     return {
-      nickname: 'what is your nickName?'
-    }
+      nickname: "what is your nickName?",
+      country: "pc-as"
+    };
   },
   methods: {
-    searchLog: function(nickname) {
+    searchLog: function() {
+      console.log("nickname", this.nickname);
       //TODO
+      // this.$http.headers.common["Authorization"] =
+      //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2NjFlZmMwMC0xOTE1LTAxMzYtZWVhZC00YjgzYjRkZTM4NzUiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyNzIyODQ0LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymdsb2ctYTM5MjkxODEtNGYxOS00N2UxLWEwZmYtYjlmOTRhNzQ1ZGMzIiwic2NvcGUiOiJjb21tdW5pdHkiLCJsaW1pdCI6MTB9.zztYrvx6FM-MFVjWvtPC-bC-hjuHqy7jbm85fBSyfpY";
+      // this.$http.headers.common["Accept"] = "application/vnd.api+json";
 
-      Vue.http.headers.common['Authorization'] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2NjFlZmMwMC0xOTE1LTAxMzYtZWVhZC00YjgzYjRkZTM4NzUiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyNzIyODQ0LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymdsb2ctYTM5MjkxODEtNGYxOS00N2UxLWEwZmYtYjlmOTRhNzQ1ZGMzIiwic2NvcGUiOiJjb21tdW5pdHkiLCJsaW1pdCI6MTB9.zztYrvx6FM-MFVjWvtPC-bC-hjuHqy7jbm85fBSyfpY";
+      let pubgApiUrl = `https://api.playbattlegrounds.com/shards/${
+        this.country
+      }/players?filter[playerNames]=${this.nickname}`;
 
-      this.$http.get(pubgApiUrl + nickname).then(response => {
+      this.$http.get(pubgApiUrl).then(response => {
         console.log("response", response);
         this.message = response.data.message;
       });
 
+      // axios
+      //   .get(pubgApiUrl + nickname, "", {
+      //     headers: {
+      //       Authorization:
+      //         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2NjFlZmMwMC0xOTE1LTAxMzYtZWVhZC00YjgzYjRkZTM4NzUiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyNzIyODQ0LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymdsb2ctYTM5MjkxODEtNGYxOS00N2UxLWEwZmYtYjlmOTRhNzQ1ZGMzIiwic2NvcGUiOiJjb21tdW5pdHkiLCJsaW1pdCI6MTB9.zztYrvx6FM-MFVjWvtPC-bC-hjuHqy7jbm85fBSyfpY",
+      //       Accept: "application/vnd.api+json"
+      //     }
+      //   })
+      //   .then(response => {
+      //     console.log("response", response);
+      //     this.message = response.data.message;
+      //   });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -49,5 +70,8 @@ li {
 }
 a {
   color: #42b983;
+}
+h1 {
+  color: white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,19 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'vue-awesome/icons'
-import Icon from 'vue-awesome/components/Icon'
+import VueResource from 'vue-resource'
 
-Vue.component('icon', Icon)
 Vue.config.productionTip = false
+Vue.use(VueResource)
+
+Vue.http.interceptors.push((request, next) => {
+  request.headers.set(
+    'Authorization',
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2NjFlZmMwMC0xOTE1LTAxMzYtZWVhZC00YjgzYjRkZTM4NzUiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyNzIyODQ0LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymdsb2ctYTM5MjkxODEtNGYxOS00N2UxLWEwZmYtYjlmOTRhNzQ1ZGMzIiwic2NvcGUiOiJjb21tdW5pdHkiLCJsaW1pdCI6MTB9.zztYrvx6FM-MFVjWvtPC-bC-hjuHqy7jbm85fBSyfpY'
+  )
+  request.headers.set('Accept', 'application/vnd.api+json')
+  next()
+})
 
 /* eslint-disable no-new */
 new Vue({
