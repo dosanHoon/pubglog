1f17e4ba1ad5b12a5ab13406505b5ee3e778b1db
danedehaan <danedehaan@qubitsec.com>
2018-07-02 13:06:30 +0900

    pubgid 구하는 api 별도로 구현

--- server/router/api/seasonStats.js
+++ server/router/api/seasonStats.js
@@ -3,54 +3,30 @@ var serverPrivate = require('../../serverprivate')
 
 module.exports = function (app) {
   app.get('/seasonStats', (req, res) => {
-    const { country, season, nickname } = req.query
+    const { country, season, pubgId } = req.query
 
     let options = {
       host: 'api.playbattlegrounds.com',
-      path: `/shards/${country}/players?filter[playerNames]=${nickname}`,
+      path: `/shards/${country}/players/${pubgId}/seasons/${season}`,
       method: 'GET',
       headers: {
         Accept: 'application/vnd.api+json',
         Authorization: serverPrivate.authKey
       }
     }
-    let result = []
-    let resultForId
-    let id
+    let resultStat = []
+    let resultForResponse
+
     https
       .request(options, function (response) {
         response
           .on('data', function (data) {
-            result.push(data)
+            resultStat.push(data)
           })
           .on('end', function () {
-            let data = Buffer.concat(result).toString()
-            resultForId = JSON.parse(data)
-            id = resultForId.data.map(({ id }) => id)
-            console.log('id', id[0])
-
-            options.path = `/shards/${country}/players/${
-              id[0]
-            }/seasons/${season}`
-
-            console.log('options.path', options.path)
-
-            let resultStat = []
-            let resultForResponse
-
-            https
-              .request(options, function (response) {
-                response
-                  .on('data', function (data) {
-                    resultStat.push(data)
-                  })
-                  .on('end', function () {
-                    let data = Buffer.concat(resultStat).toString()
-                    resultForResponse = JSON.parse(data)
-                    res.json(resultForResponse.data.attributes.gameModeStats)
-                  })
-              })
-              .end()
+            let data = Buffer.concat(resultStat).toString()
+            resultForResponse = JSON.parse(data)
+            res.json(resultForResponse.data.attributes.gameModeStats)
           })
       })
       .end()
--- server/router/api/user.js
+++ server/router/api/user.js
@@ -1,3 +1,5 @@
+var https = require('https')
+var serverPrivate = require('../../serverprivate')
 var User = require('../../mongoose').User
 
 module.exports = function (app) {
@@ -16,5 +18,49 @@ module.exports = function (app) {
       console.log('saved successfully ' + newUser.item)
       res.redirect('/')
     })
+  })
+
+  app.get('/pubgId', (req, res) => {
+    const { country, nickname } = req.query
+
+    let options = {
+      host: 'api.playbattlegrounds.com',
+      path: `/shards/${country}/players?filter[playerNames]=${nickname}`,
+      method: 'GET',
+      headers: {
+        Accept: 'application/vnd.api+json',
+        Authorization: serverPrivate.authKey
+      }
+    }
+    let result = []
+    let resultForId
+    let id
+    let resposeResult = {
+      returnCode: 0,
+      resultValue: ''
+    }
+
+    https
+      .request(options, function (response) {
+        response
+          .on('data', function (data) {
+            result.push(data)
+          })
+          .on('end', function () {
+            let data = Buffer.concat(result).toString()
+            resultForId = JSON.parse(data)
+            if (resultForId.data) {
+              id = resultForId.data.map(({ id }) => id)
+              resposeResult.resultValue = id[0]
+              res.json(resposeResult)
+            } else if (resultForId.errors) {
+              resposeResult.returnCode = -100
+              resposeResult.resultValue = resultForId.errors[0]
+
+              res.json(resposeResult)
+            }
+          })
+      })
+      .end()
   })
 }
