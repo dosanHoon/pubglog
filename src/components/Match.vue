42f59ca9cc704ace200201aa93dc38354f063fee
danedehaan <danedehaan@qubitsec.com>
2018-07-11 17:28:53 +0900

    최신경기 컴포넌트

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -1,35 +1,93 @@
 <template>
-    <li>
-        <p>{{userStat.name}}</p>
-        <ul>
-        <li>킬 : {{userStat.kills}}</li>
-        <li>데미지 : {{userStat.damageDealt.toFixed(0)}}</li>
-        <li>기절 : {{userStat.DBNOs}}</li>
-        <li>헤드샷 : {{userStat.headshotKills}}</li>
-        </ul>
+    <li class="match_container">
+      <div class="match_info_wrap item_column">
+        <p>{{mapName}}</p>
+        <p>{{matchInfo.data.attributes.gameMode}}</p>
+        <p>{{matchInfo.data.attributes.duration}}</p>
+        <p>{{matchInfo.data.attributes.createdAt}}</p>
+      </div>
+      <template v-if="userStats">
+        <div class="match_info_wrap item_column">
+          <p>{{userStats.winPlace}} : 등</p>
+          <p>{{userStats.kills}} : 킬</p>
+        </div>  
+        <div class="match_info_wrap item_column">
+          <p>데미지 : {{userStats.damageDealt.toFixed(0)}}</p>
+        </div>
+      </template>
     </li>
 </template>
 
 <script>
 export default {
-  name: "Match",
-  props: ["userStat"]
-};
+  name: 'Match',
+  data() {
+    return {
+      // 팀정보: [],
+      // 유저들정보: []
+    }
+  },
+  props: ['matchInfo', 'pubgId'],
+  computed: {
+    mapName: function() {
+      return this.matchInfo.data.attributes.mapName.replace('_Main', '')
+    },
+    팀정보: function() {
+      console.log('팀정보')
+      let result = []
+      this.matchInfo.included.forEach(data => {
+        if (data.type === 'roster') {
+          result.push(data)
+        }
+      })
+
+      return result
+    },
+    유저들정보: function() {
+      let result = []
+      this.matchInfo.included.forEach(data => {
+        if (data.type == 'participant') {
+          result.push(data)
+        }
+      })
+
+      return result
+    },
+    userStats: function() {
+      let result
+
+      this.유저들정보.forEach((data, index) => {
+        let id = data.attributes.stats.playerId
+        if (id === this.pubgId) {
+          result = data.attributes.stats
+        }
+      })
+      return result
+    }
+  },
+  created() {
+    console.log('created')
+  }
+}
 </script>
 
 <style scoped>
-ul {
+.match_container {
   background: #dde5d0;
-  padding: 30px;
+  padding: 10px;
   color: black;
+  border-left: 4px solid #629e60;
 }
-ul:after {
-  clear: both;
-  display: block;
-  content: "";
+.item_column {
+  display: table-cell;
+  vertical-align: middle;
 }
-li ul li {
-  width: 50%;
-  float: left;
+.match_info_wrap {
+  width: 82px;
+  padding-left: 12px;
+  text-align: center;
+}
+p {
+  margin: 0;
 }
 </style>
